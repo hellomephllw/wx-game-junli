0d01f550186e826a75ef07ba5fc91d5b2d51b675
hellomephllw <hellomephllw@163.com>
2017-06-22 00:09:30 +0800

    finish table index ui

--- web/public/scripts/bg-game/index.js
+++ web/public/scripts/bg-game/index.js
@@ -1,3 +1,27 @@
 /**
  * Created by liliwen on 2017/6/21.
  */
+var options = {
+    currentPage: 3,
+    totalPages: 10,
+    size:'normal',
+    alignment:'right',
+    tooltipTitles: function (type, page, current) {
+        switch (type) {
+            case "first":
+                return "Tooltip for first page";
+            case "prev":
+                return "Tooltip for previous page";
+            case "next":
+                return "Tooltip for next page";
+            case "last":
+                return "Tooltip for last page";
+            case "page":
+                return "Tooltip for page " + page;
+        }
+    }
+}
+
+$('#paging').bootstrapPaginator(options);
+
+console.log(1);
