7adc232671889fed9651b96f9b8bf460123b4085
hellomephllw <hellomephllw@163.com>
2017-06-22 00:23:21 +0800

    optimize table index ui

--- web/public/scripts/bg-game/index.js
+++ web/public/scripts/bg-game/index.js
@@ -5,23 +5,7 @@ var options = {
     currentPage: 3,
     totalPages: 10,
     size:'normal',
-    alignment:'right',
-    tooltipTitles: function (type, page, current) {
-        switch (type) {
-            case "first":
-                return "Tooltip for first page";
-            case "prev":
-                return "Tooltip for previous page";
-            case "next":
-                return "Tooltip for next page";
-            case "last":
-                return "Tooltip for last page";
-            case "page":
-                return "Tooltip for page " + page;
-        }
-    }
-}
+    alignment:'right'
+};
 
 $('#paging').bootstrapPaginator(options);
-
-console.log(1);
