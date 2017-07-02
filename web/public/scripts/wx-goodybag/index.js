c114d49c77e4143e245ff317cce148e36a0c6a35
hellomephllw <hellomephllw@163.com>
2017-07-03 02:20:05 +0800

    finish ui of wx page

---
+++ web/public/scripts/wx-goodybag/index.js
@@ -0,0 +1,21 @@
+/**
+ * Created by liliwen on 2017/7/2.
+ */
+(function() {
+    var wxIndexPage = {
+        init: function() {
+
+        },
+        initData: function() {
+
+        },
+        initPage: function() {
+
+        },
+        initEvent: function() {
+
+        }
+    };
+
+    wxIndexPage.init();
+}());
